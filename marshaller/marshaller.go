e6572214badde2f29159a419035d635e07242a1d
Josh <JoshTsai@trevi.cc>
2022-10-27 10:34:14 +0800

    add: marshaller

---
+++ marshaller/marshaller.go
@@ -0,0 +1,45 @@
+package marshaller
+
+import (
+	"encoding/json"
+	"reflect"
+	"time"
+)
+
+func Marshal[T struct{}](s *T, fieldTag string, seperator string) string {
+	key := ""
+
+	v := reflect.ValueOf(s)
+	t := reflect.TypeOf(s)
+	for i := 0; i < v.Elem().NumField(); i++ {
+
+		if t.Elem().Field(i).Tag == "" {
+			continue
+		}
+
+		tag, ok := t.Elem().Field(i).Tag.Lookup(fieldTag)
+		if !ok {
+			continue
+		}
+		if v.Elem().Field(i).IsNil() {
+			continue
+		}
+
+		value := v.Elem().Field(i).Interface()
+
+		if t, ok := value.(time.Time); ok {
+			value = t.Unix()
+		}
+		if t, ok := value.(*time.Time); ok {
+			value = t.Unix()
+		}
+
+		if key != "" {
+			key += seperator
+		}
+
+		valueMarshalled, _ := json.Marshal(value)
+		key += tag + seperator + string(valueMarshalled)
+	}
+	return key
+}
